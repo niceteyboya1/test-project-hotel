9b42e7aec6557704bc7301cda07dc0625dd8d19c
housir0 <2143554397@qq.com>
2025-07-15 17:56:53 +0800

    no.3

---
+++ hotel/hotel-back/src/main/java/cn/gok/hotel/mapper/RoomMapper.java
@@ -0,0 +1,8 @@
+package cn.gok.hotel.mapper;
+
+import cn.gok.hotel.entity.Room;
+import java.util.List;
+
+public interface RoomMapper {
+    List<Room> findList6();
+}
---
+++ hotel/hotel-back/src/main/java/cn/gok/hotel/service/RoomTypeService.java
@@ -0,0 +1,8 @@
+package cn.gok.hotel.service;
+
+import cn.gok.hotel.entity.RoomType;
+import java.util.List;
+
+public interface RoomTypeService {
+    List<RoomType> findTop6();
+}
